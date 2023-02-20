a9e31bb9f268cf6e54f5d8ab34b440a471ac484f
albert-de-montserrat <albert.de.montserrat@gmail.com>
2023-02-20 14:18:52 +0100

    bugfix

--- src/Utils.jl
+++ src/Utils.jl
@@ -25,7 +25,7 @@ end
 # broadcast getindex() to NamedTuples
 @inline function ntuple_idx(args::NamedTuple, I::Vararg{Integer,N}) where {N}
     k = keys(args)
-    v = getindex.(values(args), I)
+    v = getindex.(values(args), (I)...)
     return (; zip(k, v)...)
 end
 
